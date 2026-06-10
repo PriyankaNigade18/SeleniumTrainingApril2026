4176ad4971f7f4ffe844cc1fa6eedcd3f5ccdf07
PriyankaNigade18 <piyu1818@gmail.com>
2026-06-10 13:19:45 +0530

    Selenium WebDriver Setup

---
+++ src/test/java/com/WebTesting/Tc001_FirstTest.java
@@ -0,0 +1,17 @@
+package com.WebTesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Tc001_FirstTest {
+
+	public static void main(String[] args) 
+	{
+		//create driver session with chrome
+		
+		WebDriver driver=new ChromeDriver();
+		
+
+	}
+
+}
